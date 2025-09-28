c94f07c66d5ba39af88ecdf9fa729a3b4386f082
Sebastian Weber <sebastian-laurin.weber@outlook.com>
2025-09-28 18:40:30 +0200

    CORS added and frontend added

--- backend.py
+++ backend.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, send_file, jsonify
+from flask_cors import CORS
 from get_tabs import crawl_tabs
 import os
 import tempfile
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/generate", methods=["POST"])
 def generate():
